81173a7648d0bb6dc44581996fed746f4989d51c
zlcomcn <105707290+zlcomcn@users.noreply.github.com>
2023-12-22 10:17:37 +0800

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -31,7 +31,7 @@ else
 fi
 echo "架构: ${arch}"
 
-last_version=$(curl -Ls "https://api.github.com/repos/4xmen/radepa-x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+last_version=$(curl -Ls "https://api.github.com/repos/sing-web/x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 if [[ -z "$last_version" ]]; then
     red "检测 x-ui 版本失败，可能是超出 Github API 限制，正在使用备用源检测最新版本"
     last_version=$(curl -sm8 https://github.com/4xmen/radepa-x-ui/raw/main/config/version)
@@ -41,7 +41,7 @@ if [[ -z "$last_version" ]]; then
     fi
 fi
 echo -e "${yellow}检测到 x-ui 最新版本：${last_version}，开始安装${plain}"
-curl -Ls https://github.com/4xmen/radepa-x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz -o x-ui-linux-${arch}.tar.gz
+curl -Ls https://github.com/sing-web/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz -o x-ui-linux-${arch}.tar.gz
 tar zxvf x-ui-linux-${arch}.tar.gz -C /usr/local && rm x-ui-linux-${arch}.tar.gz -rf
 chmod +x /usr/local/x-ui/x-ui /usr/local/x-ui/bin/xray-linux-*
 
